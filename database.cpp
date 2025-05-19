19045cabe41978ebab683c51915817e37cace455
D3zNt <nicolascps4@gmail.com>
2025-05-19 20:10:30 +0700

    binary file

--- database.cpp
+++ database.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <thread> 
+#include <chrono>
+#include <ctime>
+#include <iomanip> 
+
 using namespace std;
 
 const int SIZE = 10;
@@ -13,13 +18,13 @@ struct CPU_DATA{
 
 void search();
 void sort();
-void convert_to_bson();
+void convert_to_binary(CPU_DATA dataList[], int counter);
 void convert_to_json();
 
 
 
 int main(){
-    const CPU_DATA tempList[10] = { // temporary later on we will try to connect with realtime data
+    CPU_DATA tempList[10] = { // temporary later on we will try to connect with realtime data
         {1, 99.4, 85.3},
         {2, 12.3, 86.2},
         {3, 34.3, 73.2},
@@ -32,15 +37,41 @@ int main(){
         {10, 82.3, 8.2},
     };
 
-    // FUNC list
 
-    // simulated data (the CPU data will change in XX time, simulate real time)
-    // search
-    // sorting 
-    // convert to BSON
-    // convert to JSON
+    // need multithreading  one thread is still here saving the file
+    int counter = 1;
+    while (true) {
+        convert_to_binary(tempList, counter);
+        counter++;
+        std::this_thread::sleep_for(std::chrono::seconds(10));  
+    }
+
+    
 }
 
 
 
+void convert_to_binary(CPU_DATA dataList[],int counter){
+    
+    fstream f("records.dat", ios::out | ios::binary);
 
+    if(f){
+        f.write(reinterpret_cast<char*>(dataList), SIZE * sizeof(CPU_DATA));
+        f.close();      
+    }else {
+        cout << "failed to write";
+    }
+    
+    // f.open("records.dat", ios::in | ios::binary);
+    // if(f){
+
+    //     CPU_DATA temp[SIZE];
+    //     f.read(reinterpret_cast<char*>(temp), SIZE * sizeof(CPU_DATA));
+    //     f.close();
+
+
+    //     for (CPU_DATA& r : temp){
+    //         cout << r.cpu << ", " << r.id << ", " << r.memory << endl;
+    //     }
+    // }
+}
