246d99d6db10b50d2c327037de330a96e2189a9e
saddam.titanio <saddam.titanio25@gmail.com>
2025-05-20 14:41:40 +0700

    Parsed the data into JSON, created a global vector to store all the records for the current session, set up template code for future functionalities

---
+++ include/dataToJson.hpp
@@ -0,0 +1,12 @@
+#ifndef DATA_TO_JSON_HPP
+#define DATA_TO_JSON_HPP
+
+#include <vector>
+#include <database.hpp>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <database.hpp>
+
+void convertToJSON();
+
+#endif
